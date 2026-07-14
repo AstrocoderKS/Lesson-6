724c42cab28c77dfd91eb139cd3f9bf6bb2b904f
AstrocoderKS <astroks26@gmail.com>
2026-07-14 19:46:31 +1000

    Operation

---
+++ Operator.py
@@ -0,0 +1,16 @@
+a = 50
+b = 50
+c = 90
+if a != b:
+    print("a and b are not the same.")
+if b != c:
+    print("b and c are not the same.")
+if c != a:
+    print("c and a are not the same.")
+d = "Snake"
+e = "Bird"
+f = "Fish"
+if d == e:
+    print("d and e are the same.")
+else:
+    print("d and e are totally irrelevant.")
