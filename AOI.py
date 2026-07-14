4164ece363435f41b31e90d65dc7a56edec72a6d
AstrocoderKS <astroks26@gmail.com>
2026-07-14 19:20:13 +1000

    AOI

---
+++ AOI.py
@@ -0,0 +1,11 @@
+a = 40
+b = 30
+c = 60
+if a >b and a >c:
+    print("a is greatest of all.")
+else:
+    print("a is not the greatest of all.")
+if a >b or a >c:
+    print("a is equivalent to being the largest.")
+else:
+    print("a is more miniscular than one of these.")
