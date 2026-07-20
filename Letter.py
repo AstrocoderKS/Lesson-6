6805837d55f9c9baa237d697c2bd4f163d5ac902
AstrocoderKS <astroks26@gmail.com>
2026-07-20 18:45:39 +1000

    Alphabet.py

---
+++ Letter.py
@@ -0,0 +1,6 @@
+# Radhika wants to create a program to check if the character the user enters is an alphabet or not.
+A = (input("Please enter your value here."))
+if ('a' <= A <= 'z'):
+    print("This value is an alphabet.")
+else:
+    print("This value is not an alphabet.")
